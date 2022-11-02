183bb0eaaea992b3b24b9d77890e113acf0bff16
Antara Titikhsha Barua <22400005+antitikhsha@users.noreply.github.com>
2022-11-02 13:56:34 +0600

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,4 @@
+#In this program, we will take two integer numbers from user, and print the result
+x=int(input('first number:'))
+y=int(input('second number:'))
+print('addition:',(x+y))
---
+++ example3.py
@@ -0,0 +1,9 @@
+#In this program, we will compare two integer values
+x=20
+y=10
+if x>y:
+    print('x is greater than y')
+elif x<y:
+    print('x is less than y')
+else:
+    print('both are equal')
